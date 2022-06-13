bad68d81850672a85298c7d82f927a9acc244440
Anukul Rawat <anukulmickey9@gmail.com>
2022-06-13 22:36:50 +0530

    Updates Document Title

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const AppBar = () => {
+
+    useEffect(() => {
+        document.title = 'Trello App'; // Set the document title
+    }, [])
+
     return (
         <div className="App-header">
             <span className='PageTitle'>Trello-Clone</span>
